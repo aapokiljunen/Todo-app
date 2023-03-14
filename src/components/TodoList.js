6f84ef07763d75fdcde0ffae898be8ed45ba3798
Aapo <aapo.kiljunen@gmail.com>
2023-03-14 21:49:25 +0200

    MUI-tehtavia

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,15 +3,40 @@ import { useRef, useState } from "react";
 import { AgGridReact } from 'ag-grid-react'
 import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-material.css';
+import DeleteIcon from '@mui/icons-material/Delete'
+import { Tabs } from "@mui/material";
+import Tab from "@mui/material/Tab";
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import Stack from '@mui/system/Stack';
+import dayjs from "dayjs";
+import 'dayjs/locale/fi';
+import { DatePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+
+
+
 
 function TodoList() {
+    const [value, setValue] = useState('home');
+
+    const handleChange = (event, value) => {
+        setValue(value);
+    };
+
     const gridOptions = {
         columnDefs: [
-            { field: "date", headerName: "Päivämäärä" },
+            {
+                field: "date", headerName: "Päivämäärä", valueFormatter: (params) => {
+                    return dayjs(params.data.date).format("DD.MM.YYYY");
+                }
+            },
             { field: "desc", headerName: "Tehtävä" },
-            { field: "priority", headerName: "Tärkeys",
+            {
+                field: "priority", headerName: "Tärkeys",
                 cellStyle: params => params.value.toLowerCase() === "high" ? { color: 'red' } : { color: 'black' }
-            }, 
+            },
         ],
         defaultColDef: {
             sortable: true,
@@ -19,7 +44,7 @@ function TodoList() {
             floatingFilter: true,
         },
         animateRows: true,
-        rowSelection: "single" 
+        rowSelection: "single"
     }
 
     const [todoLine, setTodoLine] = useState({ desc: '', date: '', priority: '' });
@@ -33,6 +58,10 @@ function TodoList() {
         setTodos([...todos, todoLine]);
     }
 
+    const yourChangeDateFunc = (selectedDate) => {
+        setTodoLine({ ...todoLine, date: selectedDate });
+    }
+
     const deleteTodo = () => {
         if (gridRef.current.getSelectedNodes().length > 0) {
             setTodos(todos.filter((todoLine, i) => i !== gridRef.current.getSelectedNodes()[0].childIndex));
@@ -43,22 +72,35 @@ function TodoList() {
     };
 
     return (
-        <div>
-            <div>ToDo-lista</div>
-            <div>
-                <input type="text" name="desc" placeholder="Päivämäärä" onChange={inputChanged} value={todoLine.desc} />
-                <input type="text" name="date" placeholder="Tehtävä" onChange={inputChanged} value={todoLine.date} />
-                <input type="text" name="priority" placeholder="Tärkeys" onChange={inputChanged} value={todoLine.priority} />
-                <button onClick={addTodo}>Lisää</button>
-                <button onClick={deleteTodo}>Poista</button></div>
-            <div className="ag-theme-material"
-                style={{ height: '700px', width: '70%', margin: 'auto' }} >
-                <AgGridReact
-                    gridOptions={gridOptions}
-                    rowData={todos}
-                    ref={gridRef}
-                    onGridReady={params => gridRef.current = params.api} />
-            </div>
+        <div className="App">
+            <Tabs value={value} onChange={handleChange}>
+                <Tab value="home" label="Home" />
+                <Tab value="todo" label="Todo-list" />
+            </Tabs>
+            {value === 'home' && <div>Heipä hei</div>}
+            {value === 'todo' && <div className="todopage">
+                <div className="control-fields">
+                    <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
+                        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fi">
+                            <DatePicker value={todoLine.date} onChange={date => yourChangeDateFunc(date)} />
+                        </LocalizationProvider>
+                        <TextField name="desc" variant="standard" label="Tehtävä" onChange={inputChanged} value={todoLine.desc} />
+                        <TextField name="priority" variant="standard" label="Tärkeys" onChange={inputChanged} value={todoLine.priority} />
+                        <Button onClick={addTodo} variant="contained">Lisää</Button>
+                        <Button onClick={deleteTodo} variant="outlined" startIcon={<DeleteIcon />}>Poista</Button>
+                    </Stack>
+                </div>
+                <div className="ag-theme-material"
+                    style={{ height: '700px', width: '70%', margin: 'auto' }} >
+                    <AgGridReact
+                        gridOptions={gridOptions}
+                        rowData={todos}
+                        ref={gridRef}
+                        onGridReady={params => gridRef.current = params.api} />
+                </div>
+            </div>}
+
+
         </div>
     );
 }
